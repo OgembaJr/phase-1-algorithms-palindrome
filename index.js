46fb805661a211508f008486b8fe6a90191069ea
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2022-12-15 12:13:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const palindromeWord =  word.split("").reverse().join("");
+  return word === palindromeWord
 }
-
 /* 
   Add your pseudocode here
 */
+
+// set a variable to take in the word that is reversed 
+// split the word so that it turns into an array
+// reverse the array
+// string the array by use of join method
+// Do a comparison to the reversed word
+// return true if they match and false otherwise
 
 /*
   Add written explanation of your solution here
